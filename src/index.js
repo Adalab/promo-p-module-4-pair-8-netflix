2dcade68f9ccfe447ed8e3306c19de658af2de38
GemaMesasV <gema.mesas.v@gmail.com>
2022-05-12 11:15:43 +0200

    create frontend endpoint

--- src/index.js
+++ src/index.js
@@ -9,5 +9,6 @@ server.use(express.json());
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
+  
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
