a7382837711ed47f427228f13a01eca8f4d3196c
Veronica isla <veroislagleaniz@gmail.com>
2022-05-17 11:21:40 +0200

    exercise 1 and 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const moviesData = require("./data/movies.json");
+const moviesData = require('./data/movies.json');
 
 // create and config server
 const server = express();
@@ -10,14 +10,27 @@ server.use(express.json());
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
-  
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
+//17-05
+// 1. Añade un endpoint a src/index.js del tipo server.get('/movie/:movieId', (req, res) => { ... });.
+server.get('/movie/:movieId', (req, res) => {
+  console.log(req.params.movieId);
+
+  // 2. En ejercicios anteriores ya has importado las películas (movies) al principio de src/index.js las movies.
+  // Con un find busca en ese array de películas que has importado la que tenga el mismo id que estás recibiendo por URL params. Guárdala en una constante, por ejemplo en const foundMovie =
+
+  const foundMovie = moviesData.movies.find(
+    (movies) => movies.id === req.params.movieId
+  );
+  console.log(foundMovie);
+});
+
 //aquí empieza el codigo del ejercicio 4.2
-server.get("/movies", (req, res) => {
+server.get('/movies', (req, res) => {
   //guardamos el valor del query en una constante
-  const genderFilterParam = req.query.gender ? req.query.gender : "";
+  const genderFilterParam = req.query.gender ? req.query.gender : '';
   //aquí respondemos con el listado filtrado
   res.json({
     success: true,
@@ -28,7 +41,7 @@ server.get("/movies", (req, res) => {
       //Se compara con -1 porque en la segunda condición le estamos indicando que la cadena z o referenceStr(z-a) iría por delante de a o compareString(a-z)
       .sort(function (a, z) {
         const sortFilterParam = a.title.localeCompare(z.title);
-        if (req.query.sort === "asc") {
+        if (req.query.sort === 'asc') {
           return sortFilterParam;
         } else {
           return sortFilterParam * -1;
@@ -37,11 +50,10 @@ server.get("/movies", (req, res) => {
   });
 });
 
-const staticServerPathWeb = './src/public-react/'; 
+const staticServerPathWeb = './src/public-react/';
 // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathWeb));
 
-
-const staticServerPathWebImages = './src/public-movies-images/'; 
+const staticServerPathWebImages = './src/public-movies-images/';
 // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathWebImages));
